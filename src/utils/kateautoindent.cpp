9b0df4d1b94ba6a958a42495596d1e505a944bd4
Christoph Cullmann <cullmann@kde.org>
2014-07-26 16:21:36 +0200

    fix output

--- src/utils/kateautoindent.cpp
+++ src/utils/kateautoindent.cpp
@@ -339,7 +339,7 @@ void KateAutoIndent::setMode(const QString &name)
             return;
         } else {
             qCWarning(LOG_PART) << "mode" << name <<
-                                "requires a different highlight style: document style '" << doc->highlightingMode() << "'"
+                                "requires a different highlight style: document style '" << doc->highlight()->style() << "'"
                                 ", but script require '" << script->indentHeader().requiredStyle() << "'"
                                 ;
         }
@@ -356,7 +356,7 @@ void KateAutoIndent::checkRequiredStyle()
     if (m_script) {
         if (!isStyleProvided(m_script, doc->highlight())) {
             qCDebug(LOG_PART) << "mode" << m_mode <<
-                              "requires a different highlight style: document style '" << doc->highlightingMode() << "'"
+                              "requires a different highlight style: document style '" << doc->highlight()->style() << "'"
                               ", but script require '" << m_script->indentHeader().requiredStyle() << "'"
                               ;
             doc->config()->setIndentationMode(MODE_NORMAL);
