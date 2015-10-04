a036088a9f219f39967e46ade9703419c3e8afab
Andrew Chen <andrew.chuanye.chen@gmail.com>
2015-10-04 13:16:51 +0200

    Fix Kate word count issues by listening to DocumentPrivate instead of Document
    REVIEW: 125354
    BUG: 353258

--- src/view/wordcounter.cpp
+++ src/view/wordcounter.cpp
@@ -33,7 +33,7 @@ WordCounter::WordCounter(KTextEditor::ViewPrivate *view)
     , m_document(view->document())
 {
     connect(view->doc(), &KTextEditor::DocumentPrivate::textInserted, this, &WordCounter::textInserted);
-    connect(m_document, &KTextEditor::Document::textRemoved, this, &WordCounter::textRemoved);
+    connect(view->doc(), &KTextEditor::DocumentPrivate::textRemoved, this, &WordCounter::textRemoved);
     connect(view->doc(), &KTextEditor::DocumentPrivate::loaded, this, &WordCounter::recalculate);
     connect(view, &KTextEditor::View::selectionChanged, this, &WordCounter::selectionChanged);
 
