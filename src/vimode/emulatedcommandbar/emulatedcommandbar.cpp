ac0e86530578ef7665ecc8135a77df2d6b221feb
Simon St James <kdedevel@etotheipiplusone.com>
2016-06-17 09:50:12 +0100

    Add/ update licensing info for Emulated Command Bar stuff.

--- src/vimode/emulatedcommandbar/emulatedcommandbar.cpp
+++ src/vimode/emulatedcommandbar/emulatedcommandbar.cpp
@@ -1,6 +1,6 @@
 /*  This file is part of the KDE libraries and the Kate part.
  *
- *  Copyright (C) 2013 Simon St James <kdedevel@etotheipiplusone.com>
+ *  Copyright (C) 2013-2016 Simon St James <kdedevel@etotheipiplusone.com>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
