KDE/ktexteditor

commit 9223ac51c9453017c9b11718eed858050697cfbd

cleanup logic for searchbar creation
based on patch provided in linked review request
::searchBar now uses enum and triggers state changes only if required

REVIEW: 128615

I think the enum class is not needed, won't make this more "nice" and is not supported
by all Windows compilers (< MSVC 2015)

src/inputmode/katenormalinputmode.cpp
@@ -128,9 +128,7 @@ void KateNormalInputMode::readWriteChanged(bool)
 
 void KateNormalInputMode::find()
 {
-    const bool INIT_HINT_AS_INCREMENTAL = false;
-    KateSearchBar *const bar = searchBar(INIT_HINT_AS_INCREMENTAL);
-    bar->enterIncrementalMode();
+    KateSearchBar *const bar = searchBar(IncrementalSearchBar);
     view()->bottomViewBar()->addBarWidget(bar);
     view()->bottomViewBar()->showBarWidget(bar);
     bar->setFocus();
@@ -148,9 +146,7 @@ void KateNormalInputMode::findSelectedBackwards()
 
 void KateNormalInputMode::findReplace()
 {
-    const bool INIT_HINT_AS_POWER = true;
-    KateSearchBar *const bar = searchBar(INIT_HINT_AS_POWER);
-    bar->enterPowerMode();
+    KateSearchBar *const bar = searchBar(PowerSearchBar);
     view()->bottomViewBar()->addBarWidget(bar);
     view()->bottomViewBar()->showBarWidget(bar);
     bar->setFocus();
@@ -158,12 +154,12 @@ void KateNormalInputMode::findReplace()
 
 void KateNormalInputMode::findNext()
 {
-    searchBar()->findNext();
+    searchBar(IncrementalSearchBarOrKeepMode)->findNext();
 }
 
 void KateNormalInputMode::findPrevious()
 {
-    searchBar()->findPrevious();
+    searchBar(IncrementalSearchBarOrKeepMode)->findPrevious();
 }
 
 void KateNormalInputMode::activateCommandLine()
@@ -180,11 +176,30 @@ void KateNormalInputMode::activateCommandLine()
     cmdLineBar()->setFocus();
 }
 
-KateSearchBar *KateNormalInputMode::searchBar(bool initHintAsPower)
+KateSearchBar *KateNormalInputMode::searchBar(const SearchBarMode mode)
 {
+    /**
+     * power mode wanted?
+     */
+    const bool wantPowerMode = (mode == PowerSearchBar);
+
+    /**
+     * create search bar as not there? use right mode
+     */
     if (!m_searchBar) {
-        m_searchBar = new KateSearchBar(initHintAsPower, view(), KateViewConfig::global());
+        m_searchBar = new KateSearchBar(wantPowerMode, view(), KateViewConfig::global());
     }
+
+    /**
+     * else: switch mode if needed!
+     */
+    else if ((mode != IncrementalSearchBarOrKeepMode) && (wantPowerMode != m_searchBar->isPower())) {
+        if (wantPowerMode)
+            m_searchBar->enterPowerMode();
+        else
+            m_searchBar->enterIncrementalMode();
+    }
+
     return m_searchBar;
 }
 
@@ -196,11 +211,6 @@ KateCommandLineBar *KateNormalInputMode::cmdLineBar()
     }
 
     return m_cmdLine;
-}
-
-bool KateNormalInputMode::hasSearchBar() const
-{
-    return m_searchBar != 0;
 }
 
 void KateNormalInputMode::updateRendererConfig()

src/inputmode/katenormalinputmode.h
@@ -76,8 +76,38 @@ public:
     QString bookmarkLabel(int line) const Q_DECL_OVERRIDE;
 
 private:
-    KateSearchBar *searchBar(bool initHintAsPower = false);
-    bool hasSearchBar() const;
+    /**
+     * Search bar mode:
+     *   - Incremental mode
+     *   - Power mode, aka find & replace
+     *   - Incremental mode, but don't change mode if already there
+     */
+    enum SearchBarMode {
+        IncrementalSearchBar,
+        PowerSearchBar,
+        IncrementalSearchBarOrKeepMode
+    };
+
+    /**
+     * Get search bar, create it on demand. (with right mode)
+     * @param mode wanted search bar mode
+     * @return search bar widget
+     */
+    KateSearchBar *searchBar(const SearchBarMode mode);
+
+    /**
+     * search bar around?
+     * @return search bar around?
+     */
+    bool hasSearchBar() const
+    {
+        return m_searchBar;
+    }
+
+    /**
+     * Get command line bar, create it on demand.
+     * @return command line bar, created if not already there
+     */
     KateCommandLineBar *cmdLineBar();
 
 private:

commit 2c4feeb0c9107732399f8ae3dacea3124572f345

KateNormalInputMode needs to rerun SearchBar enter methods
This is a fix for a regression resulting from commit
9223ac51c9453017c9b11718eed858050697cfbd.

REVIEW: 128859

src/inputmode/katenormalinputmode.cpp
@@ -184,7 +184,7 @@ KateSearchBar *KateNormalInputMode::searchBar(const SearchBarMode mode)
     const bool wantPowerMode = (mode == PowerSearchBar);
 
     /**
-     * create search bar as not there? use right mode
+     * create search bar is not there? use right mode
      */
     if (!m_searchBar) {
         m_searchBar = new KateSearchBar(wantPowerMode, view(), KateViewConfig::global());
@@ -193,11 +193,12 @@ KateSearchBar *KateNormalInputMode::searchBar(const SearchBarMode mode)
     /**
      * else: switch mode if needed!
      */
-    else if ((mode != IncrementalSearchBarOrKeepMode) && (wantPowerMode != m_searchBar->isPower())) {
-        if (wantPowerMode)
+    else if (mode != IncrementalSearchBarOrKeepMode) {
+        if (wantPowerMode) {
             m_searchBar->enterPowerMode();
-        else
+        } else {
             m_searchBar->enterIncrementalMode();
+        }
     }
 
     return m_searchBar;

src/inputmode/katenormalinputmode.h
@@ -78,9 +78,9 @@ public:
 private:
     /**
      * Search bar mode:
-     *   - Incremental mode
-     *   - Power mode, aka find & replace
-     *   - Incremental mode, but don't change mode if already there
+     *   - Setup Incremental mode, among other things: potential new search pattern
+     *   - Setup Power mode, aka find & replace: Also potential new search pattern
+     *   - Use current mode and current search pattern or if no Search bar exists, launch Incremental mode
      */
     enum SearchBarMode {
         IncrementalSearchBar,