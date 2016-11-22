81e98e4527bf7880f27236ad32abeb8940165dd3
Burkhard Lück <lueck@hube-lueck.de>
2016-11-22 19:31:56 +0100

    fix message extraction, syntax xml files moved to frameworks/syntaxhighlighting

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp || exit 11
-$EXTRACTATTR --attr=language,name,Language --attr="language,section,Language Section" syntax/data/*.xml >> rc.cpp || exit 12
+#$EXTRACTATTR --attr=language,name,Language --attr="language,section,Language Section" syntax/data/*.xml >> rc.cpp || exit 12
 grep -n -e '^ *"name":' script/data/indentation/*.js | sed 's!^\(.*\):.*"name": *"\(.*\)".*$!// i18n: file: \1\ni18nc("Autoindent mode", "\2");!' | sed 's/ \+")/")/' >>rc.cpp || exit 13
 grep -n -e '^ *"name":' script/data/commands/*.js | sed 's!^\(.*\):.*"name": *"\(.*\)".*$!// i18n: file: \1\ni18nc("Script command name", "\2");!' | sed 's/ \+")/")/' >>rc.cpp || exit 13
 grep -n -e '^ *"category":' script/data/commands/*.js | sed 's!^\(.*\):.*"category": *"\(.*\)".*$!// i18n: file: \1\ni18nc("Script command category", "\2");!' | sed 's/ \+")/")/' >>rc.cpp || exit 13
