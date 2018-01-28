2d6a2b517868ff7ff4168feb327c499359d1721f
Christoph Cullmann <cullmann@kde.org>
2018-01-28 09:23:15 +0100

    improve line heigh computation try to avoid cutting of parts of glyphs
    Differential Revision: https://phabricator.kde.org/D10145

--- src/render/katerenderer.cpp
+++ src/render/katerenderer.cpp
@@ -946,10 +946,9 @@ void KateRenderer::updateConfig()
 
 void KateRenderer::updateFontHeight()
 {
-    // use height of font + round down, ensure we have at least one pixel
-    // we round down to avoid artifacts: line height too large vs. qt background rendering of text attributes
-    const qreal height = config()->fontMetrics().height();
-    m_fontHeight = qMax(1, qFloor(height));
+    // ensure minimal height of one pixel to not fall in the div by 0 trap somewhere
+    // use font line spacing, this includes the leading
+    m_fontHeight = qMax(1, qCeil(config()->fontMetrics().lineSpacing()));
 }
 
 void KateRenderer::updateMarkerSize()
@@ -1030,6 +1029,9 @@ void KateRenderer::layoutLine(KateLineLayoutPtr lineLayout, int maxwidth, bool c
         {
             line.setLineWidth(maxwidth);
         }
+
+        // we include the leading, this must match the ::updateFontHeight code!
+        line.setLeadingIncluded(true);
 
         line.setPosition(QPoint(line.lineNumber() ? shiftX : 0, height));
 
