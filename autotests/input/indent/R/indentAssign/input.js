4c17dde71e2021ab78fdc75aa48f003f652893f7
Christoph Cullmann <cullmann@kde.org>
2018-09-08 17:55:30 +0200

    Unit tests for the R indent script
    Summary: A series of unit tests as was request in this [previous review of the R indent script](https://phabricator.kde.org/D12587).
    
    Test Plan: The tests were... well, tested using kateindenttest built from sources (master) and were running OK.
    
    Reviewers: #ktexteditor, #rkward, cullmann
    
    Reviewed By: #ktexteditor, cullmann
    
    Subscribers: cullmann, kwrite-devel, kde-frameworks-devel
    
    Tags: #kate, #frameworks
    
    Differential Revision: https://phabricator.kde.org/D15350

---
+++ autotests/input/indent/R/indentAssign/input.js
@@ -0,0 +1,8 @@
+v.setCursorPosition(0,6);
+v.enter();
+v.type("Thisisaverylongvariablename");
+v.enter();
+v.type("test <- NA");
+v.enter();
+v.type("test <- 'String'");
+v.enter();
---
+++ autotests/input/indent/R/skipComment/input.js
@@ -0,0 +1,4 @@
+v.setCursorPosition(3,16);
+v.enter();
+v.type("drop_na()");
+v.enter();
