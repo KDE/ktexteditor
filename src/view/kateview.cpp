KDE/ktexteditor

commit 743cbe13fda0f664fc622eaf5b9e1e2d6ee626e6

ViewConfig: Use new generic config interface
This is a somehow raw transition, autotests are adapted too and still working
but there may still some adjustments needed, like value validators, fix
accidentally wrong converted default settings or whatever else.

These two changes may cause some incompatibilities, too bad, but very nice
in the long term:

Use the power of new the interface in ViewPrivate::setConfigValue
Use the power of new the interface in DocumentPrivate::setViewVariable
With that we will have soon everywhere the same keys and the possibility
literally to set each setting by document variables or the F7 command line
without extra needed work for new options in the future.

Differential Revision: https://phabricator.kde.org/D20924

src/view/kateview.cpp
@@ -1374,7 +1374,7 @@ void KTextEditor::ViewPrivate::setInputMode(KTextEditor::View::InputMode mode)
     m_viewInternal->m_currentInputMode = m_viewInternal->m_inputModes[mode];
     m_viewInternal->m_currentInputMode->activate();
 
-    config()->setInputMode(mode); // TODO: this could be called from read config procedure, so it's not a good idea to set a specific view mode here
+    config()->setValue(KateViewConfig::InputMode, mode); // TODO: this could be called from read config procedure, so it's not a good idea to set a specific view mode here
 
     /* small duplication, but need to do this if not toggled by action */
     Q_FOREACH(QAction *action, m_inputModeActions->actions()) {
@@ -1459,7 +1459,7 @@ void KTextEditor::ViewPrivate::slotLostFocus()
 
 void KTextEditor::ViewPrivate::setDynWrapIndicators(int mode)
 {
-    config()->setDynWordWrapIndicators(mode);
+    config()->setValue(KateViewConfig::DynWordWrapIndicators, mode);
 }
 
 bool KTextEditor::ViewPrivate::isOverwriteMode() const
@@ -1610,10 +1610,7 @@ void KTextEditor::ViewPrivate::readSessionConfig(const KConfigGroup &config, con
     // cursor position
     setCursorPositionInternal(KTextEditor::Cursor(config.readEntry("CursorLine", 0), config.readEntry("CursorColumn", 0)));
 
-    // restore dyn word wrap if set for this view
-    if (config.hasKey("Dynamic Word Wrap")) {
-        m_config->setDynWordWrap(config.readEntry("Dynamic Word Wrap", false));
-    }
+    m_config->setDynWordWrap(config.readEntry("Dynamic Word Wrap", false));
 
     // restore text folding
     m_savedFoldingState = QJsonDocument::fromJson(config.readEntry("TextFolding", QByteArray()));
@@ -1632,10 +1629,7 @@ void KTextEditor::ViewPrivate::writeSessionConfig(KConfigGroup &config, const QS
     config.writeEntry("CursorLine", cursorPosition().line());
     config.writeEntry("CursorColumn", cursorPosition().column());
 
-    // save dyn word wrap if set for this view
-    if (m_config->dynWordWrapSet()) {
-        config.writeEntry("Dynamic Word Wrap", m_config->dynWordWrap());
-    }
+    config.writeEntry("Dynamic Word Wrap", m_config->dynWordWrap());
 
     // save text folding state
     saveFoldingState();
@@ -1684,57 +1678,57 @@ void KTextEditor::ViewPrivate::setAddBom(bool enabled)
 
 void KTextEditor::ViewPrivate::setIconBorder(bool enable)
 {
-    config()->setIconBar(enable);
+    config()->setValue(KateViewConfig::ShowIconBar, enable);
 }
 
 void KTextEditor::ViewPrivate::toggleIconBorder()
 {
-    config()->setIconBar(!config()->iconBar());
+    config()->setValue(KateViewConfig::ShowIconBar, config()->iconBar());
 }
 
 void KTextEditor::ViewPrivate::setLineNumbersOn(bool enable)
 {
-    config()->setLineNumbers(enable);
+    config()->setValue(KateViewConfig::ShowLineNumbers, enable);
 }
 
 void KTextEditor::ViewPrivate::toggleLineNumbersOn()
 {
-    config()->setLineNumbers(!config()->lineNumbers());
+    config()->setValue(KateViewConfig::ShowLineNumbers, !config()->lineNumbers());
 }
 
 void KTextEditor::ViewPrivate::setScrollBarMarks(bool enable)
 {
-    config()->setScrollBarMarks(enable);
+    config()->setValue(KateViewConfig::ShowScrollBarMarks, enable);
 }
 
 void KTextEditor::ViewPrivate::toggleScrollBarMarks()
 {
-    config()->setScrollBarMarks(!config()->scrollBarMarks());
+    config()->setValue(KateViewConfig::ShowScrollBarMarks, !config()->scrollBarMarks());
 }
 
 void KTextEditor::ViewPrivate::setScrollBarMiniMap(bool enable)
 {
-    config()->setScrollBarMiniMap(enable);
+    config()->setValue(KateViewConfig::ShowScrollBarMiniMap, enable);
 }
 
 void KTextEditor::ViewPrivate::toggleScrollBarMiniMap()
 {
-    config()->setScrollBarMiniMap(!config()->scrollBarMiniMap());
+    config()->setValue(KateViewConfig::ShowScrollBarMiniMap, !config()->scrollBarMiniMap());
 }
 
 void KTextEditor::ViewPrivate::setScrollBarMiniMapAll(bool enable)
 {
-    config()->setScrollBarMiniMapAll(enable);
+    config()->setValue(KateViewConfig::ShowScrollBarMiniMapAll, enable);
 }
 
 void KTextEditor::ViewPrivate::toggleScrollBarMiniMapAll()
 {
-    config()->setScrollBarMiniMapAll(!config()->scrollBarMiniMapAll());
+    config()->setValue(KateViewConfig::ShowScrollBarMiniMapAll, !config()->scrollBarMiniMapAll());
 }
 
 void KTextEditor::ViewPrivate::setScrollBarMiniMapWidth(int width)
 {
-    config()->setScrollBarMiniMapWidth(width);
+    config()->setValue(KateViewConfig::ScrollBarMiniMapWidth, width);
 }
 
 void KTextEditor::ViewPrivate::toggleDynWordWrap()
@@ -1760,12 +1754,12 @@ void KTextEditor::ViewPrivate::toggleWordCount(bool on)
 
 void KTextEditor::ViewPrivate::setFoldingMarkersOn(bool enable)
 {
-    config()->setFoldingBar(enable);
+    config()->setValue(KateViewConfig::ShowFoldingBar, enable);
 }
 
 void KTextEditor::ViewPrivate::toggleFoldingMarkers()
 {
-    config()->setFoldingBar(!config()->foldingBar());
+    config()->setValue(KateViewConfig::ShowFoldingBar, !config()->foldingBar());
 }
 
 bool KTextEditor::ViewPrivate::iconBorder()
@@ -2613,7 +2607,7 @@ bool KTextEditor::ViewPrivate::isAutomaticInvocationEnabled() const
 
 void KTextEditor::ViewPrivate::setAutomaticInvocationEnabled(bool enabled)
 {
-    config()->setAutomaticCompletionInvocation(enabled);
+    config()->setValue(KateViewConfig::AutomaticCompletionInvocation, enabled);
 }
 
 void KTextEditor::ViewPrivate::sendCompletionExecuted(const KTextEditor::Cursor &position, KTextEditor::CodeCompletionModel *model, const QModelIndex &index)
@@ -3306,6 +3300,15 @@ QVariant KTextEditor::ViewPrivate::configValue(const QString &key)
 
 void KTextEditor::ViewPrivate::setConfigValue(const QString &key, const QVariant &value)
 {
+    // First, try the new config interface
+    if (config()->setValue(key, value)) {
+        return;
+
+    } else if (renderer()->config()->setValue(key, value)) {
+        return;
+    }
+
+    // No success? Go the old way
     if (value.canConvert(QVariant::Color)) {
         if (key == QLatin1String("background-color")) {
             renderer()->config()->setBackgroundColor(value.value<QColor>());
@@ -3328,33 +3331,10 @@ void KTextEditor::ViewPrivate::setConfigValue(const QString &key, const QVariant
     } else if (value.type() == QVariant::Bool) {
         // Note explicit type check above. If we used canConvert, then
         // values of type UInt will be trapped here.
-        if (key == QLatin1String("icon-bar")) {
-            config()->setIconBar(value.toBool());
-        } else if (key == QLatin1String("line-numbers")) {
-            config()->setLineNumbers(value.toBool());
-        } else if (key == QLatin1String("dynamic-word-wrap")) {
+        if (key == QLatin1String("dynamic-word-wrap")) {
             config()->setDynWordWrap(value.toBool());
-        } else if (key == QLatin1String("allow-mark-menu")) {
-            config()->setAllowMarkMenu(value.toBool());
-        } else if (key == QLatin1String("folding-bar")) {
-            config()->setFoldingBar(value.toBool());
-        } else if (key == QLatin1String("folding-preview")) {
-            config()->setFoldingPreview(value.toBool());
-        } else if (key == QLatin1String("modification-markers")) {
-            config()->setLineModification(value.toBool());
-        } else if (key == QLatin1String("keyword-completion")) {
-            config()->setKeywordCompletion(value.toBool());
         } else if (key == QLatin1String("word-count")) {
             config()->setShowWordCount(value.toBool());
-        } else if (key == QLatin1String("scrollbar-minimap")) {
-            config()->setScrollBarMiniMap(value.toBool());
-        } else if (key == QLatin1String("scrollbar-preview")) {
-            config()->setScrollBarPreview(value.toBool());
-        }
-
-    } else if (value.canConvert(QVariant::UInt)) {
-        if (key == QLatin1String("default-mark-type")) {
-            config()->setDefaultMarkType(value.toUInt());
         }
 
     } else if (value.canConvert(QVariant::Font)) {

autotests/src/katedocument_test.cpp
@@ -317,7 +317,7 @@ void KateDocumentTest::testAutoBrackets()
     };
 
     doc.setHighlightingMode ("Normal"); // Just to be sure
-    view->config()->setAutoBrackets(true);
+    view->config()->setValue(KateViewConfig::AutoBrackets, true);
 
     QString testInput;
 
@@ -623,7 +623,7 @@ void KateDocumentTest::testRemoveComposedCharacters()
 {
     KTextEditor::DocumentPrivate doc;
     auto view = static_cast<KTextEditor::ViewPrivate*>(doc.createView(nullptr));
-    view->config()->setBackspaceRemoveComposed(true);
+    view->config()->setValue(KateViewConfig::BackspaceRemoveComposedCharacters, true);
     doc.setText(QString::fromUtf8("व्यक्तियों"));
     doc.del(view, Cursor(0, 0));
 

autotests/src/kateview_test.cpp
@@ -309,7 +309,7 @@ void KateViewTest::testDeselectByArrowKeys()
     QCOMPARE(view->cursorPosition(), cur2);  // Be at end: foobar|haz
     QCOMPARE(view->selection(), false);
 
-    view->config()->setPersistentSelection(true);
+    view->config()->setValue(KateViewConfig::PersistentSelection, true);
 
     view->setSelection(range);
     view->setCursorPositionInternal(cur1);
@@ -369,7 +369,7 @@ void KateViewTest::testScrollPastEndOfDocument()
     view->show();
 
     // enable "[x] Scroll past end of document"
-    view->config()->setScrollPastEnd(true);
+    view->config()->setValue(KateViewConfig::ScrollPastEnd, true);
     QCOMPARE(view->config()->scrollPastEnd(), true);
 
     // disable dynamic word wrap
@@ -403,14 +403,14 @@ void KateViewTest::testFoldFirstLine()
     QCOMPARE(doc.highlightingMode(), QString("C++"));
 
     KTextEditor::ViewPrivate *view = new KTextEditor::ViewPrivate(&doc, nullptr);
-    view->config()->setFoldFirstLine(false);
+    view->config()->setValue(KateViewConfig::FoldFirstLine, false);
     view->setCursorPosition({4, 0});
 
     // initially, nothing is folded
     QVERIFY(view->textFolding().isLineVisible(1));
 
     // now change the config, and expect the header to be folded
-    view->config()->setFoldFirstLine(true);
+    view->config()->setValue(KateViewConfig::FoldFirstLine, true);
     qint64 foldedRangeId = 0;
     QVERIFY(!view->textFolding().isLineVisible(1, &foldedRangeId));
 

autotests/src/testutils.cpp
@@ -166,7 +166,7 @@ bool KateViewObject::type(const QString &str)
 
 void KateViewObject::setAutoBrackets(bool enable)
 {
-    view()->config()->setAutoBrackets(enable);
+    view()->config()->setValue(KateViewConfig::AutoBrackets, enable);
 }
 
 

autotests/src/vimode/base.cpp
@@ -107,8 +107,8 @@ void BaseTest::init()
     vi_global = vi_input_mode->globalState();
     Q_ASSERT(vi_global);
     kate_document->config()->setShowSpaces(KateDocumentConfig::Trailing); // Flush out some issues in the KateRenderer when rendering spaces.
-    kate_view->config()->setScrollBarMiniMap(false);
-    kate_view->config()->setScrollBarPreview(false);
+    kate_view->config()->setValue(KateViewConfig::ShowScrollBarMiniMap, false);
+    kate_view->config()->setValue(KateViewConfig::ShowScrollBarPreview, false);
 
     connect(kate_document, &KTextEditor::DocumentPrivate::textInserted,
             this, &BaseTest::textInserted);

autotests/src/vimode/completion.cpp
@@ -105,7 +105,7 @@ void CompletionTest::FakeCodeCompletionTests()
 {
     // Test that FakeCodeCompletionTestModel behaves similar to the code-completion in e.g. KDevelop.
     const bool oldStealKeys = KateViewConfig::global()->viInputModeStealKeys();
-    KateViewConfig::global()->setViInputModeStealKeys(true); // For Ctrl-P, Ctrl-N etc
+    KateViewConfig::global()->setValue(KateViewConfig::ViInputModeStealKeys, true); // For Ctrl-P, Ctrl-N etc
     ensureKateViewVisible(); // KTextEditor::ViewPrivate needs to be visible for the completion widget.
     FakeCodeCompletionTestModel *fakeCodeCompletionModel = new FakeCodeCompletionTestModel(kate_view);
     kate_view->registerCompletionModel(fakeCodeCompletionModel);
@@ -432,7 +432,7 @@ void CompletionTest::FakeCodeCompletionTests()
     TestPressKey("ifun\\ctrl- \\enterX");
     FinishTest("functionCall(X);(<-old bracket");
 
-    KateViewConfig::global()->setViInputModeStealKeys(oldStealKeys);
+    KateViewConfig::global()->setValue(KateViewConfig::ViInputModeStealKeys, oldStealKeys);
     kate_view->hide();
     mainWindow->hide();
     kate_view->unregisterCompletionModel(fakeCodeCompletionModel);
@@ -443,9 +443,9 @@ void CompletionTest::CompletionTests()
 {
     const bool oldRemoveTailOnCompletion = KateViewConfig::global()->wordCompletionRemoveTail();
     // For these tests, assume we don't swallow the tail on completion.
-    KateViewConfig::global()->setWordCompletionRemoveTail(false);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, false);
 
-    KateViewConfig::global()->setViInputModeStealKeys(true); // For Ctrl-P, Ctrl-N etc
+    KateViewConfig::global()->setValue(KateViewConfig::ViInputModeStealKeys, true); // For Ctrl-P, Ctrl-N etc
     ensureKateViewVisible(); // KTextEditor::ViewPrivate needs to be visible for the completion widget.
     VimCodeCompletionTestModel *testModel = new VimCodeCompletionTestModel(kate_view);
 
@@ -554,7 +554,7 @@ void CompletionTest::CompletionTests()
     // i.e. fairly intelligently :)
     FakeCodeCompletionTestModel *fakeCodeCompletionModel = new FakeCodeCompletionTestModel(kate_view);
     fakeCodeCompletionModel->setRemoveTailOnComplete(true);
-    KateViewConfig::global()->setWordCompletionRemoveTail(true);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, true);
     kate_view->registerCompletionModel(fakeCodeCompletionModel);
     clearAllMacros();
     BeginTest("funct\nnoa\ncomtail\ncomtail");
@@ -564,13 +564,13 @@ void CompletionTest::CompletionTests()
     TestPressKey("i\\right\\right\\right\\right\\right\\ctrl- \\enterfirstArg"); // Function with args.
     TestPressKey("\\home\\down\\right\\right\\right\\ctrl- \\enter");            // Function no args.
     fakeCodeCompletionModel->setRemoveTailOnComplete(true);
-    KateViewConfig::global()->setWordCompletionRemoveTail(true);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, true);
     TestPressKey("\\home\\down\\right\\right\\right\\ctrl- \\enter");   // Cut off tail.
     fakeCodeCompletionModel->setRemoveTailOnComplete(false);
-    KateViewConfig::global()->setWordCompletionRemoveTail(false);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, false);
     TestPressKey("\\home\\down\\right\\right\\right\\ctrl- \\enter\\ctrl-c");   // Don't cut off tail.
     fakeCodeCompletionModel->setRemoveTailOnComplete(true);
-    KateViewConfig::global()->setWordCompletionRemoveTail(true);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, true);
     // Replay.
     fakeCodeCompletionModel->setFailTestOnInvocation(true);
     kate_document->setText("funct\nnoa\ncomtail\ncomtail");
@@ -590,7 +590,7 @@ void CompletionTest::CompletionTests()
 
     kate_view->unregisterCompletionModel(fakeCodeCompletionModel);
     delete fakeCodeCompletionModel;
-    KateViewConfig::global()->setWordCompletionRemoveTail(oldRemoveTailOnCompletion);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, oldRemoveTailOnCompletion);
 
     // Hide the kate_view for subsequent tests.
     kate_view->hide();

autotests/src/vimode/emulatedcommandbarsetupandteardown.cpp
@@ -68,7 +68,7 @@ EmulatedCommandBarSetUpAndTearDown::EmulatedCommandBarSetUpAndTearDown(KateViInp
     while (QApplication::hasPendingEvents()) {
         QApplication::processEvents();
     }
-    KateViewConfig::global()->setViInputModeStealKeys(true);
+    KateViewConfig::global()->setValue(KateViewConfig::ViInputModeStealKeys, true);
     m_window->installEventFilter(&m_windowKeepActive);
 }
 EmulatedCommandBarSetUpAndTearDown::~EmulatedCommandBarSetUpAndTearDown()
@@ -78,7 +78,7 @@ EmulatedCommandBarSetUpAndTearDown::~EmulatedCommandBarSetUpAndTearDown()
     QMetaObject::invokeMethod(m_viInputMode->viModeEmulatedCommandBar(), "hideMe");
     m_view->hide();
     m_window->hide();
-    KateViewConfig::global()->setViInputModeStealKeys(false);
+    KateViewConfig::global()->setValue(KateViewConfig::ViInputModeStealKeys, false);
     while (QApplication::hasPendingEvents()) {
         QApplication::processEvents();
     }

autotests/src/vimode/keys.cpp
@@ -45,7 +45,7 @@ void KeysTest::MappingTests()
 //     QVERIFY(false);
     const int mappingTimeoutMSOverride = QString::fromLocal8Bit(qgetenv("KATE_VIMODE_TEST_MAPPINGTIMEOUTMS")).toInt();
     const int mappingTimeoutMS = (mappingTimeoutMSOverride > 0) ? mappingTimeoutMSOverride : 2000;
-    KateViewConfig::global()->setViInputModeStealKeys(true); // For tests involving e.g. <c-a>
+    KateViewConfig::global()->setValue(KateViewConfig::ViInputModeStealKeys, true); // For tests involving e.g. <c-a>
     {
         // Check storage and retrieval of mapping recursion.
         clearAllMappings();
@@ -592,7 +592,7 @@ void KeysTest::MappingTests()
 void KeysTest::LeaderTests()
 {
     // Clean slate.
-    KateViewConfig::global()->setViInputModeStealKeys(true);
+    KateViewConfig::global()->setValue(KateViewConfig::ViInputModeStealKeys, true);
     clearAllMappings();
 
     // By default the backslash character is the leader. The default leader
@@ -662,7 +662,7 @@ void KeysTest::AltGr()
     // Alt-gr+7 (that is: Alt-gr down; "{"; Alt-gr up).
 
     // Ensure we have auto brackets off, or the test will fail
-    kate_view->config()->setAutoBrackets(false);
+    kate_view->config()->setValue(KateViewConfig::AutoBrackets, false);
 
     BeginTest("");
     TestPressKey("i");
@@ -946,7 +946,7 @@ void KeysTest::MacroTests()
     ensureKateViewVisible();
     // Want Vim mode to intercept ctrl-p, ctrl-n shortcuts, etc.
     const bool oldStealKeys = KateViewConfig::global()->viInputModeStealKeys();
-    KateViewConfig::global()->setViInputModeStealKeys(true);
+    KateViewConfig::global()->setValue(KateViewConfig::ViInputModeStealKeys, true);
 
     // Don't invoke completion via ctrl-space when replaying a macro.
     clearAllMacros();
@@ -1095,7 +1095,7 @@ void KeysTest::MacroTests()
     // Note that there is no way (in general) to determine if a completion was
     // non-tail-removing, so we explicitly set the config to false.
     const bool oldRemoveTailOnCompletion = KateViewConfig::global()->wordCompletionRemoveTail();
-    KateViewConfig::global()->setWordCompletionRemoveTail(false);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, false);
     const bool oldReplaceTabsDyn = kate_document->config()->replaceTabsDyn();
     kate_document->config()->setReplaceTabsDyn(false);
     fakeCodeCompletionModel->setRemoveTailOnComplete(false);
@@ -1121,7 +1121,7 @@ void KeysTest::MacroTests()
     FinishTest("(123_completionATail");
 
     // Correctly remove word if we are set to remove tail.
-    KateViewConfig::global()->setWordCompletionRemoveTail(true);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, true);
     clearAllMacros();
     BeginTest("(123_compTail)");
     fakeCodeCompletionModel->setCompletions({ "123_completionA" });
@@ -1152,10 +1152,10 @@ void KeysTest::MacroTests()
     fakeCodeCompletionModel->setCompletions({ "123_completionA" });
     fakeCodeCompletionModel->setFailTestOnInvocation(false);
     fakeCodeCompletionModel->setRemoveTailOnComplete(true);
-    KateViewConfig::global()->setWordCompletionRemoveTail(true);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, true);
     TestPressKey("qqfTi\\ctrl- \\enter\\ctrl-cq");
     fakeCodeCompletionModel->setFailTestOnInvocation(true);
-    KateViewConfig::global()->setWordCompletionRemoveTail(false);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, false);
     kate_document->setText("(123_compTail_456)");
     TestPressKey("gg@q");
     FinishTest("(123_completionA)");
@@ -1164,10 +1164,10 @@ void KeysTest::MacroTests()
     fakeCodeCompletionModel->setCompletions({ "123_completionA" });
     fakeCodeCompletionModel->setFailTestOnInvocation(false);
     fakeCodeCompletionModel->setRemoveTailOnComplete(false);
-    KateViewConfig::global()->setWordCompletionRemoveTail(false);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, false);
     TestPressKey("qqfTi\\ctrl- \\enter\\ctrl-cq");
     fakeCodeCompletionModel->setFailTestOnInvocation(true);
-    KateViewConfig::global()->setWordCompletionRemoveTail(true);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, true);
     kate_document->setText("(123_compTail_456)");
     TestPressKey("gg@q");
     FinishTest("(123_completionATail_456)");
@@ -1178,10 +1178,10 @@ void KeysTest::MacroTests()
     fakeCodeCompletionModel->setCompletions({ "123_completionA" });
     fakeCodeCompletionModel->setFailTestOnInvocation(false);
     fakeCodeCompletionModel->setRemoveTailOnComplete(true);
-    KateViewConfig::global()->setWordCompletionRemoveTail(true);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, true);
     TestPressKey("qqfTi\\ctrl- \\enter\\ctrl-c");
     fakeCodeCompletionModel->setRemoveTailOnComplete(false);
-    KateViewConfig::global()->setWordCompletionRemoveTail(false);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, false);
     TestPressKey("j^fTi\\ctrl- \\enter\\ctrl-cq");
     kate_document->setText("(123_compTail_456)\n(123_compTail_456)");
     TestPressKey("gg@q");
@@ -1203,7 +1203,7 @@ void KeysTest::MacroTests()
     fakeCodeCompletionModel->setCompletions({ "123_completionA" });
     fakeCodeCompletionModel->setFailTestOnInvocation(false);
     fakeCodeCompletionModel->setRemoveTailOnComplete(false);
-    KateViewConfig::global()->setWordCompletionRemoveTail(false);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, false);
     TestPressKey("qqi\\ctrl- \\enter\\ctrl-cq");
     kate_document->setText("oldwordshouldbeuntouched");
     fakeCodeCompletionModel->setFailTestOnInvocation(true);
@@ -1216,7 +1216,7 @@ void KeysTest::MacroTests()
     fakeCodeCompletionModel->setCompletions({ "123_completionA" });
     fakeCodeCompletionModel->setFailTestOnInvocation(false);
     fakeCodeCompletionModel->setRemoveTailOnComplete(true);
-    KateViewConfig::global()->setWordCompletionRemoveTail(true);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, true);
     TestPressKey("qqi\\ctrl- \\enter\\ctrl-cq");
     kate_document->setText("oldwordshouldbedeleted");
     fakeCodeCompletionModel->setFailTestOnInvocation(true);
@@ -1226,7 +1226,7 @@ void KeysTest::MacroTests()
     // Completion of functions.
     // Currently, not removing the tail on function completion is not supported.
     fakeCodeCompletionModel->setRemoveTailOnComplete(true);
-    KateViewConfig::global()->setWordCompletionRemoveTail(true);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, true);
     // A completed, no argument function "function()" is repeated correctly.
     BeginTest("");
     fakeCodeCompletionModel->setCompletions({ "function()" });
@@ -1365,7 +1365,7 @@ void KeysTest::MacroTests()
     BeginTest("funct");
     fakeCodeCompletionModel->setCompletions({ "function(...)" });
     fakeCodeCompletionModel->setFailTestOnInvocation(false);
-    KateViewConfig::global()->setWordCompletionRemoveTail(false);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, false);
     TestPressKey("qqfta\\ctrl- \\enterfirstArg\\ctrl-cq");
     kate_document->setText("functxyz");
     fakeCodeCompletionModel->setFailTestOnInvocation(true);
@@ -1374,13 +1374,13 @@ void KeysTest::MacroTests()
     BeginTest("funct");
     fakeCodeCompletionModel->setCompletions({ "function()" });
     fakeCodeCompletionModel->setFailTestOnInvocation(false);
-    KateViewConfig::global()->setWordCompletionRemoveTail(false);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, false);
     TestPressKey("qqfta\\ctrl- \\enter\\ctrl-cq");
     kate_document->setText("functxyz");
     fakeCodeCompletionModel->setFailTestOnInvocation(true);
     TestPressKey("gg@q");
     FinishTest("function()");
-    KateViewConfig::global()->setWordCompletionRemoveTail(true);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, true);
 
     // Deal with cases where completion ends with ";".
     BeginTest("");
@@ -1455,13 +1455,13 @@ void KeysTest::MacroTests()
         TestPressKey("qafta\\ctrl- \\enterfirstArg\\ctrl-c"); // Function with args.
         TestPressKey("\\enterea\\ctrl- \\enter\\ctrl-c");     // Function no args.
         fakeCodeCompletionModel->setRemoveTailOnComplete(true);
-        KateViewConfig::global()->setWordCompletionRemoveTail(true);
+        KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, true);
         TestPressKey("\\enterfti\\ctrl- \\enter\\ctrl-c");   // Cut off tail.
         fakeCodeCompletionModel->setRemoveTailOnComplete(false);
-        KateViewConfig::global()->setWordCompletionRemoveTail(false);
+        KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, false);
         TestPressKey("\\enterfti\\ctrl- \\enter\\ctrl-cq");   // Don't cut off tail.
         fakeCodeCompletionModel->setRemoveTailOnComplete(true);
-        KateViewConfig::global()->setWordCompletionRemoveTail(true);
+        KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, true);
         // Record 'b'.
         fakeCodeCompletionModel->setCompletions({ "completionB", "semicolonfunctionnoargs();", "semicolonfunctionwithargs(...);" });
         TestPressKey("\\enterqbea\\ctrl- \\enter\\ctrl-cosemicolonfunctionw\\ctrl- \\enterX\\ctrl-cosemicolonfunctionn\\ctrl- \\enterX\\ctrl-cq");
@@ -1516,7 +1516,7 @@ void KeysTest::MacroTests()
     FinishTest("completionMacro completionRepeatLastChange completionRepeatLastChange");
     }
 
-    KateViewConfig::global()->setWordCompletionRemoveTail(oldRemoveTailOnCompletion);
+    KateViewConfig::global()->setValue(KateViewConfig::WordCompletionRemoveTail, oldRemoveTailOnCompletion);
     kate_document->config()->setReplaceTabsDyn(oldReplaceTabsDyn);
 
     kate_view->unregisterCompletionModel(fakeCodeCompletionModel);
@@ -1525,7 +1525,7 @@ void KeysTest::MacroTests()
     // Hide the kate_view for subsequent tests.
     kate_view->hide();
     mainWindow->hide();
-    KateViewConfig::global()->setViInputModeStealKeys(oldStealKeys);
+    KateViewConfig::global()->setValue(KateViewConfig::ViInputModeStealKeys, oldStealKeys);
 }
 
 void KeysTest::MarkTests()

commit dcc996121da2a6c96a5e1a47399a447b6d5c29e8

Fix action "Show Icon Border" to toggle border again
Regression from 743cbe13fda0f664fc622eaf5b9e1e2d6ee626e6

src/view/kateview.cpp
@@ -1706,7 +1706,7 @@ void KTextEditor::ViewPrivate::setIconBorder(bool enable)
 
 void KTextEditor::ViewPrivate::toggleIconBorder()
 {
-    config()->setValue(KateViewConfig::ShowIconBar, config()->iconBar());
+    config()->setValue(KateViewConfig::ShowIconBar, !config()->iconBar());
 }
 
 void KTextEditor::ViewPrivate::setLineNumbersOn(bool enable)