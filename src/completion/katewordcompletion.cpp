8d0f6ff8014cdf32be6410efa184a968ce53525a
Christoph Cullmann <cullmann@kde.org>
2019-08-25 11:51:48 +0200

    improve word completion to use highlighting to detect word boundaries
    Summary:
    This allows better completion e.g. for Perl or PHP
    
    BUG: 360340
    
    Test Plan:
    Open some Perl script.
    
    Type:
    
    lala_test = 1
    
    $lala = 2
    
    Now type
    
    $lal
    
    You shall now get only "lala" as proposal
    
    Reviewers: dhaumann
    
    Reviewed By: dhaumann
    
    Subscribers: kwrite-devel, kde-frameworks-devel
    
    Tags: #kate, #frameworks
    
    Differential Revision: https://phabricator.kde.org/D23410

--- src/completion/katewordcompletion.cpp
+++ src/completion/katewordcompletion.cpp
@@ -27,6 +27,7 @@
 #include "kateglobal.h"
 #include "katepartdebug.h"
 #include "katedefaultcolors.h"
+#include "katehighlight.h"
 
 #include <ktexteditor/movingrange.h>
 #include <ktexteditor/range.h>
@@ -212,6 +213,7 @@ QStringList KateWordCompletionModel::allMatches(KTextEditor::View *view, const K
     QSet<QString> result;
     const int minWordSize = qMax(2, qobject_cast<KTextEditor::ViewPrivate *>(view)->config()->wordCompletionMinimalWordLength());
     const int lines = view->document()->lines();
+    KateHighlighting *h = qobject_cast<KTextEditor::ViewPrivate *>(view)->doc()->highlight();
     for (int line = 0; line < lines; line++) {
         const QString &text = view->document()->line(line);
         int wordBegin = 0;
@@ -221,7 +223,7 @@ QStringList KateWordCompletionModel::allMatches(KTextEditor::View *view, const K
         while (offset < end) {
             const QChar c = text.at(offset);
             // increment offset when at line end, so we take the last character too
-            if ((! c.isLetterOrNumber() && c != QLatin1Char('_')) || (offset == end - 1 && offset++)) {
+            if (!h->isInWord(c) || (offset == end - 1 && offset++)) {
                 if (offset - wordBegin > minWordSize && (line != range.end().line() || offset != range.end().column())) {
                     /**
                      * don't add the word we are inside with cursor!
@@ -247,14 +249,13 @@ void KateWordCompletionModel::executeCompletionItem (KTextEditor::View *view
 ) const
 {
     KTextEditor::ViewPrivate *v = qobject_cast<KTextEditor::ViewPrivate *> (view);
+    KateHighlighting *h = v->doc()->highlight();
     if (v->config()->wordCompletionRemoveTail()) {
         int tailStart = word.end().column();
         const QString &line = view->document()->line(word.end().line());
         int tailEnd = line.length();
         for (int i = word.end().column(); i < tailEnd; ++i) {
-            // Letters, numbers and underscore are part of a word!
-            /// \todo Introduce configurable \e word-separators??
-            if (!line[i].isLetterOrNumber() && line[i] != QLatin1Char('_')) {
+            if (!h->isInWord(line[i])) {
                 tailEnd = i;
             }
         }
@@ -294,9 +295,10 @@ KTextEditor::Range KateWordCompletionModel::completionRange(KTextEditor::View *v
     int col = position.column();
 
     KTextEditor::Document *doc = view->document();
+    KateHighlighting *h = qobject_cast<KTextEditor::ViewPrivate *>(view)->doc()->highlight();
     while (col > 0) {
         const QChar c = (doc->characterAt(KTextEditor::Cursor(line, col - 1)));
-        if (c.isLetterOrNumber() || c.isMark() || c == QLatin1Char('_')) {
+        if (h->isInWord(c)) {
             col--;
             continue;
         }
