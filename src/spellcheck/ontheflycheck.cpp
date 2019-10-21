5c1b12982eb25fdf8b2b94e53b6875c7320d4bc7
Christoph Cullmann <cullmann@kde.org>
2019-10-21 20:53:08 +0200

    clarify license, add SPDX-License-Identifier

--- src/spellcheck/ontheflycheck.cpp
+++ src/spellcheck/ontheflycheck.cpp
@@ -1,27 +1,23 @@
-/*  This file is part of the KDE libraries and the Kate part.
- *
- *  Copyright (C) 2008-2010 by Michel Ludwig <michel.ludwig@kdemail.net>
- *  Copyright (C) 2009 by Joseph Wenninger <jowenn@kde.org>
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Library General Public
- *  License as published by the Free Software Foundation; either
- *  version 2 of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Library General Public License for more details.
- *
- *  You should have received a copy of the GNU Library General Public License
- *  along with this library; see the file COPYING.LIB.  If not, write to
- *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *  Boston, MA 02110-1301, USA.
- */
+/*  SPDX-License-Identifier: LGPL-2.0-or-later
 
-/* If ever threads should be used again, thread communication and
- * synchronization ought to be done with blocking queued signal connections.
- */
+    Copyright (C) 2008-2010 by Michel Ludwig <michel.ludwig@kdemail.net>
+    Copyright (C) 2009 by Joseph Wenninger <jowenn@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
 
 #include "ontheflycheck.h"
 
