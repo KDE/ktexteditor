705406bbaf0548e2bde5314fa982e7b8a480eb5c
Christoph Cullmann <cullmann@kde.org>
2019-10-21 20:30:31 +0200

    clarify license, add SPDX-License-Identifier

--- src/include/ktexteditor/sessionconfiginterface.h
+++ src/include/ktexteditor/sessionconfiginterface.h
@@ -1,22 +1,23 @@
-/* This file is part of the KDE libraries
-   Copyright (C) 2001-2014 Christoph Cullmann <cullmann@kde.org>
-   Copyright (C) 2005-2014 Dominik Haumann (dhaumann@kde.org)
-   Copyright (C) 2009 Michel Ludwig (michel.ludwig@kdemail.net)
+/*  SPDX-License-Identifier: LGPL-2.0-or-later
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
+    Copyright (C) 2001-2014 Christoph Cullmann <cullmann@kde.org>
+    Copyright (C) 2005-2014 Dominik Haumann (dhaumann@kde.org)
+    Copyright (C) 2009 Michel Ludwig (michel.ludwig@kdemail.net)
 
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
 */
 
 #ifndef KTEXTEDITOR_SESSIONCONFIGINTERFACE_H
