b6ebf3b61b8396909a33e670efd5fd74de8371e2
Christoph Cullmann <cullmann@kde.org>
2019-10-24 18:50:51 +0200

    relicense to lgplv2+, got ok of sven + michal

--- src/include/ktexteditor/inlinenoteprovider.h
+++ src/include/ktexteditor/inlinenoteprovider.h
@@ -1,26 +1,24 @@
-/* This file is part of the KDE libraries
+/*  SPDX-License-Identifier: LGPL-2.0-or-later
 
-   Copyright 2018 Sven Brauch <mail@svenbrauch.de>
-   Copyright 2018 Michal Srb <michalsrb@gmail.com>
+    Copyright 2018 Sven Brauch <mail@svenbrauch.de>
+    Copyright 2018 Michal Srb <michalsrb@gmail.com>
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) version 3, or any
-   later version accepted by the membership of KDE e.V. (or its
-   successor approved by the membership of KDE e.V.), which shall
-   act as a proxy defined in Section 6 of version 3 of the license.
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
 */
+
 #ifndef KTEXTEDITOR_INLINENOTEPROVIDER_H
 #define KTEXTEDITOR_INLINENOTEPROVIDER_H
 
