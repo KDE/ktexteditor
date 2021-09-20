KDE/ktexteditor

commit 6507add26075ffd56e2efb4a35edcf059ec5d895

Do not cancel mouse selection when using the keyboard
Instead follow through with the selection after the key was pressed.
This applies to different actions like page up, page down, home, end, up
and down.

src/view/kateview.cpp
@@ -3057,9 +3057,9 @@ void KTextEditor::ViewPrivate::shiftCursorRight()
 void KTextEditor::ViewPrivate::wordLeft()
 {
     if (currentTextLine().isRightToLeft()) {
-        m_viewInternal->wordNext();
+        m_viewInternal->wordNext(m_viewInternal->isUserSelecting());
     } else {
-        m_viewInternal->wordPrev();
+        m_viewInternal->wordPrev(m_viewInternal->isUserSelecting());
     }
 }
 
@@ -3075,9 +3075,9 @@ void KTextEditor::ViewPrivate::shiftWordLeft()
 void KTextEditor::ViewPrivate::wordRight()
 {
     if (currentTextLine().isRightToLeft()) {
-        m_viewInternal->wordPrev();
+        m_viewInternal->wordPrev(m_viewInternal->isUserSelecting());
     } else {
-        m_viewInternal->wordNext();
+        m_viewInternal->wordNext(m_viewInternal->isUserSelecting());
     }
 }
 
@@ -3092,7 +3092,7 @@ void KTextEditor::ViewPrivate::shiftWordRight()
 
 void KTextEditor::ViewPrivate::home()
 {
-    m_viewInternal->home();
+    m_viewInternal->home(m_viewInternal->isUserSelecting());
 }
 
 void KTextEditor::ViewPrivate::shiftHome()
@@ -3102,7 +3102,7 @@ void KTextEditor::ViewPrivate::shiftHome()
 
 void KTextEditor::ViewPrivate::end()
 {
-    m_viewInternal->end();
+    m_viewInternal->end(m_viewInternal->isUserSelecting());
 }
 
 void KTextEditor::ViewPrivate::shiftEnd()
@@ -3112,7 +3112,7 @@ void KTextEditor::ViewPrivate::shiftEnd()
 
 void KTextEditor::ViewPrivate::up()
 {
-    m_viewInternal->cursorUp();
+    m_viewInternal->cursorUp(m_viewInternal->isUserSelecting());
 }
 
 void KTextEditor::ViewPrivate::shiftUp()
@@ -3122,7 +3122,7 @@ void KTextEditor::ViewPrivate::shiftUp()
 
 void KTextEditor::ViewPrivate::down()
 {
-    m_viewInternal->cursorDown();
+    m_viewInternal->cursorDown(m_viewInternal->isUserSelecting());
 }
 
 void KTextEditor::ViewPrivate::shiftDown()
@@ -3162,7 +3162,7 @@ void KTextEditor::ViewPrivate::shiftBottomOfView()
 
 void KTextEditor::ViewPrivate::pageUp()
 {
-    m_viewInternal->pageUp();
+    m_viewInternal->pageUp(m_viewInternal->isUserSelecting());
 }
 
 void KTextEditor::ViewPrivate::shiftPageUp()
@@ -3172,7 +3172,7 @@ void KTextEditor::ViewPrivate::shiftPageUp()
 
 void KTextEditor::ViewPrivate::pageDown()
 {
-    m_viewInternal->pageDown();
+    m_viewInternal->pageDown(m_viewInternal->isUserSelecting());
 }
 
 void KTextEditor::ViewPrivate::shiftPageDown()

src/view/kateviewinternal.cpp
@@ -3968,6 +3968,11 @@ void KateViewInternal::viewSelectionChanged()
     m_selectionCached.setStart(KTextEditor::Cursor::invalid());
 }
 
+bool KateViewInternal::isUserSelecting() const
+{
+    return m_selChangedByUser;
+}
+
 KateLayoutCache *KateViewInternal::cache() const
 {
     return m_layoutCache;

src/view/kateviewinternal.h
@@ -212,6 +212,8 @@ public:
         return m_leftBorder;
     }
 
+    bool isUserSelecting() const;
+
     // EVENT HANDLING STUFF - IMPORTANT
 private:
     void fixDropEvent(QDropEvent *event);

commit 30f6cf190708a576f69090b8dc240960e6eac8e7

The user is not selecting as soon as we clearSelection
This is something we only realised as soon as we started making more use
of this property. A way I found to track the regression in 6507add2 was
to use ctrl+backspace to remove a word. This would leave it in selecting
mode and the user would end up selecting even though shift or anything
is pressed.

src/view/kateview.cpp
@@ -2287,6 +2287,7 @@ bool KTextEditor::ViewPrivate::clearSelection(bool redraw, bool finishedChanging
         Q_EMIT selectionChanged(this);
     }
 
+    m_viewInternal->m_selChangedByUser = false;
     // be done
     return true;
 }