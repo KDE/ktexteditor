3378e655e2269b43d8f3348dea563cca8ee21c51
Waqar Ahmed <waqar.17a@gmail.com>
2021-11-14 00:50:58 +0500

    Introduce PCH support

---
+++ src/pch.h
@@ -0,0 +1,25 @@
+#ifdef __cplusplus
+
+#include <QHash>
+#include <QList>
+#include <QMap>
+#include <QPointer>
+#include <QSharedData>
+#include <QSharedPointer>
+#include <QString>
+#include <QStringList>
+#include <QTextFormat>
+#include <QTimer>
+#include <QVariant>
+#include <QVector>
+#include <QWidget>
+
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#endif
