a77f7a9df131ac6cd2a6d6b5fb6ce9b790d689ae
Ahmad Samir <a.samirh78@gmail.com>
2021-12-30 14:10:38 +0200

    KParts::PartBase::setPluginLoadingMode() has been deprecated
    GIT_SILENT

--- src/document/katedocument.cpp
+++ src/document/katedocument.cpp
@@ -60,6 +60,8 @@
 #include <KToggleAction>
 #include <KXMLGUIFactory>
 
+#include <kparts_version.h>
+
 #include <QApplication>
 #include <QClipboard>
 #include <QCryptographicHash>
@@ -203,7 +205,9 @@ KTextEditor::DocumentPrivate::DocumentPrivate(bool bSingleViewMode, bool bReadOn
 
 {
     // no plugins from kparts here
+#if KPARTS_VERSION < QT_VERSION_CHECK(5, 90, 0)
     setPluginLoadingMode(DoNotLoadPlugins);
+#endif
 
     // setup component name
     const auto &aboutData = EditorPrivate::self()->aboutData();
