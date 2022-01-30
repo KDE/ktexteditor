7c91ca8c11a6c520508ad4f6c1b5c65c5327faf0
Waqar Ahmed <waqar.17a@gmail.com>
2022-01-30 18:44:49 +0500

    Improve wayland support for doc tip
    move() doesn't work and Qt::Tool makes us crash. Fix for now:
    - Use Qt::Tooltip
    - Use a combination of hide() + move + show() to force a move on
      wayland

--- src/completion/documentation_tip.cpp
+++ src/completion/documentation_tip.cpp
@@ -1,5 +1,7 @@
 #include "documentation_tip.h"
 
+#include <KWindowSystem>
+
 #include <QDebug>
 #include <QHBoxLayout>
 #include <QScreen>
@@ -9,7 +11,7 @@ DocTip::DocTip(QWidget *parent)
     , m_textView(new QTextBrowser(this))
 {
     setFocusPolicy(Qt::NoFocus);
-    setWindowFlags(Qt::Tool | Qt::FramelessWindowHint);
+    setWindowFlags(Qt::ToolTip | Qt::BypassGraphicsProxyWidget);
 
     m_textView->setFrameStyle(QFrame::Box | QFrame::Raised);
 
@@ -64,14 +66,28 @@ void DocTip::updatePosition()
     auto parentRight = parent->geometry().right();
     constexpr int Margin = 8;
 
+    const bool wayland = KWindowSystem::isPlatformWayland();
+
+    int x = 0;
     // is the completion widget too far right?
     if ((parentRight + this->width()) > screenRight) {
         // let's hope there is enough available space to the left of parent
-        const int x = (parent->x() - this->width()) - Margin;
-        move(x, parent->y());
+        x = (parent->x() - this->width()) - Margin;
     } else {
         // we have enough space on the right
-        const int x = parent->x() + parent->width() + Margin;
-        move(x, parent->y());
+        x = parent->x() + parent->width() + Margin;
+    }
+
+    if (x != this->x()) {
+        // On way land move() doesn't work, but if hide and then show again
+        // window is positioned correctly. This is a hack, and if there is
+        // a better solution, we can replace it
+        if (wayland) {
+            hide();
+            move(x, parent->y());
+            show();
+        } else {
+            move(x, parent->y());
+        }
     }
 }
