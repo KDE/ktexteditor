8fc9a01ca6ea70883cb7a4ad5250534f24833a12
Eric Armbruster <eric1@armbruster-online.de>
2022-07-20 19:50:59 +0200

    Move QuickDialog class from Kate here

---
+++ src/dialogs/quickdialog.h
@@ -0,0 +1,45 @@
+/*
+    SPDX-FileCopyrightText: 2021 Waqar Ahmed <waqar.17a@gmail.com>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#ifndef QUICKDIALOG_H
+#define QUICKDIALOG_H
+
+#include <QLineEdit>
+#include <QMenu>
+#include <QPointer>
+#include <QSortFilterProxyModel>
+#include <QTreeView>
+
+#include "kateprivate_export.h"
+
+class QAbstractItemModel;
+namespace KTextEditor
+{
+class MainWindow;
+}
+
+class KATE_PRIVATE_EXPORT QuickDialog : public QMenu
+{
+    Q_OBJECT
+public:
+    QuickDialog(QWidget *parent, QWidget *mainWindow);
+
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
+    void updateViewGeometry();
+    void clearLineEdit();
+
+protected Q_SLOTS:
+    virtual void slotReturnPressed() = 0;
+
+protected:
+    QTreeView m_treeView;
+    QLineEdit m_lineEdit;
+
+private:
+    QPointer<QWidget> m_mainWindow;
+};
+
+#endif // QUICKDIALOG_H
