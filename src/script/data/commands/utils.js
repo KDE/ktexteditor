404a3fac18d8a320bad8ff8a666b6baabb7f1d77
Pablo Rauzy <r@uzy.me>
2022-08-07 16:44:43 +0000

    Fix cursor position after calling an each-based command, when possible
    Hello :),
    
    This is a small fix that attempt to get the cursor bask where it was after executing a command.
    
    It works by checking if the number of lines hasn't changed and if so assume that the cursor can be set to its previous location.
    
    Example without the fix (`|` marks the cursor position)
    
    ```
      - foo
      - bar|
       - baz
    ```
    
    Calling `ltrim` will result in:
    
    ```
    - foo
    - bar
    - baz|
    ```
    
    But with the fix it will be:
    
    ```
    - foo
    - bar|
    - baz
    ```
    
    This is especially useful when calling `rtrim` on a big file (to quickly get rid of some auto-indent on lines left blank), as without the fix it moves the cursor to the end of the file which is not convenient.

--- src/script/data/commands/utils.js
+++ src/script/data/commands/utils.js
@@ -424,6 +424,7 @@ function each(func)
 {
     func = __toFunc(func, 'lines');
 
+    var cursor = view.cursorPosition();
     var selection = view.selection();
     if (!selection.isValid()) {
         // use whole range
@@ -436,11 +437,15 @@ function each(func)
     var text = document.text(selection);
 
     var lines = text.split("\n");
+    oldLineCount = lines.length;
+    oldCurrentLineLength = document.lineLength(cursor.line);
     lines = func(lines);
     if ( typeof(lines) == "object" ) {
       text = lines.join("\n");
+      newLineCount = lines.length;
     } else if ( typeof(lines) == "string" ) {
       text = lines
+      newLineCount = (lines.match('\n') || []).length;
     } else {
       throw "callback function for each has to return object or array of lines";
     }
@@ -451,6 +456,12 @@ function each(func)
     document.removeText(selection);
     document.insertText(selection.start, text);
     document.editEnd();
+    if (newLineCount == oldLineCount) {
+        if (document.lineLength(cursor.line) != oldCurrentLineLength) {
+            cursor.column = document.lastColumn(cursor.line) + 1;
+        }
+        view.setCursorPosition(cursor);
+    }
 }
 
 function filter(func)
