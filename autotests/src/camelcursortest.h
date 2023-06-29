b3263b0f1524326740b2042b2ccc88cacb951c06
Friedrich W. H. Kossebau <kossebau@kde.org>
2023-06-29 19:12:46 +0200

    Add missing include guards
    NO_CHANGELOG

--- autotests/src/camelcursortest.h
+++ autotests/src/camelcursortest.h
@@ -4,6 +4,10 @@
     SPDX-FileCopyrightText: 2021 Waqar Ahmed <waqar.17a@gmail.com>
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
+
+#ifndef CAMELCURSORTEST_H
+#define CAMELCURSORTEST_H
+
 #include <QObject>
 
 namespace KTextEditor
@@ -34,3 +38,5 @@ private:
     KTextEditor::ViewPrivate *view;
     KTextEditor::DocumentPrivate *doc;
 };
+
+#endif
