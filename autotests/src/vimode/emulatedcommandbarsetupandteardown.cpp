371bb1ec6071ca4dba32a00be5ec8f8f86b62025
Christoph Cullmann <cullmann@kde.org>
2023-10-09 22:04:09 +0200

    remove more deprecated functions

--- autotests/src/vimode/emulatedcommandbarsetupandteardown.cpp
+++ autotests/src/vimode/emulatedcommandbarsetupandteardown.cpp
@@ -34,7 +34,7 @@ bool WindowKeepActive::eventFilter(QObject *object, QEvent *event)
         // will deactivate the m_mainWindow, preventing it from receiving shortcuts.
         // If we detect this, set it back to being the active window again.
         event->ignore();
-        QApplication::setActiveWindow(m_mainWindow);
+        m_mainWindow->activateWindow();
         return true;
     }
     return false;
