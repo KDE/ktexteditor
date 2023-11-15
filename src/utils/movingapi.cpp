b443e5de4c71660b8ec1ed446cff8fc2d26976f6
Waqar Ahmed <waqar.17a@gmail.com>
2023-11-15 15:37:05 +0500

    Merge moving*.cpp files into one movingapi.cpp
    These .cpp files are basically empty and are hardly ever changed. We can
    merge them into one and try to improve the compile times

--- src/utils/movingapi.cpp
+++ src/utils/movingapi.cpp
@@ -1,6 +1,5 @@
 /*
     SPDX-FileCopyrightText: 2010 Christoph Cullmann <cullmann@kde.org>
-    SPDX-FileCopyrightText: 2010-2018 Dominik Haumann <dhaumann@kde.org>
 
     Based on code of the SmartCursor/Range by:
     SPDX-FileCopyrightText: 2003-2005 Hamish Rodda <rodda@kde.org>
@@ -8,11 +7,63 @@
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
-#include "movingcursor.h"
 #include "document.h"
 #include "documentcursor.h"
+#include "movingcursor.h"
+#include "movingrange.h"
+#include "movingrangefeedback.h"
 
 using namespace KTextEditor;
+
+// BEGIN MovingRange
+MovingRange::MovingRange() = default;
+
+MovingRange::~MovingRange() = default;
+
+void MovingRange::setRange(Cursor start, Cursor end)
+{
+    // just use other function, KTextEditor::Range will handle some normalization
+    setRange(Range(start, end));
+}
+
+bool MovingRange::overlaps(const Range &range) const
+{
+    if (range.start() <= start()) {
+        return range.end() > start();
+    }
+
+    else if (range.end() >= end()) {
+        return range.start() < end();
+    }
+
+    else {
+        return contains(range);
+    }
+}
+
+QDebug KTextEditor::operator<<(QDebug s, const MovingRange *range)
+{
+    if (range) {
+        s << "[" << range->start() << " -> " << range->end() << "]";
+    } else {
+        s << "(null range)";
+    }
+    return s.space();
+}
+
+/**
+ * qDebug() stream operator. Writes this range to the debug output in a nicely formatted way.
+ * @param s debug stream
+ * @param range range to print
+ * @return debug stream
+ */
+QDebug KTextEditor::operator<<(QDebug s, const MovingRange &range)
+{
+    return s << &range;
+}
+// END MovingRange
+
+// BEGIN MovingCursor
 
 MovingCursor::MovingCursor()
 {
@@ -116,3 +167,34 @@ QDebug KTextEditor::operator<<(QDebug s, const MovingCursor &cursor)
 {
     return s << &cursor;
 }
+// END MovingCursor
+
+// BEGIN MovingRangeFeedback
+MovingRangeFeedback::MovingRangeFeedback() = default;
+
+MovingRangeFeedback::~MovingRangeFeedback() = default;
+
+void MovingRangeFeedback::rangeEmpty(MovingRange *)
+{
+}
+
+void MovingRangeFeedback::rangeInvalid(MovingRange *)
+{
+}
+
+void MovingRangeFeedback::mouseEnteredRange(MovingRange *, View *)
+{
+}
+
+void MovingRangeFeedback::mouseExitedRange(MovingRange *, View *)
+{
+}
+
+void MovingRangeFeedback::caretEnteredRange(MovingRange *, View *)
+{
+}
+
+void MovingRangeFeedback::caretExitedRange(MovingRange *, View *)
+{
+}
+// END MovingRangeFeedback
