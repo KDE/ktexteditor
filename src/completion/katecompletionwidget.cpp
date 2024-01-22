ed7644d6d0c5aa8d075d5b8bbc3a684b21726fad
Waqar Ahmed <waqar.17a@gmail.com>
2024-01-22 21:31:27 +0500

    Fix double click broken in completion widget

--- src/completion/katecompletionwidget.cpp
+++ src/completion/katecompletionwidget.cpp
@@ -179,7 +179,8 @@ void KateCompletionWidget::viewFocusOut()
 {
     QWidget *toplevels[3] = {m_entryList, m_docTip, m_argumentHintWidget};
     if (!std::any_of(std::begin(toplevels), std::end(toplevels), [](QWidget *w) {
-            return w->isAncestorOf(QApplication::focusWidget());
+            auto fw = QApplication::focusWidget();
+            return fw == w || w->isAncestorOf(w);
         })) {
         abortCompletion();
     }
