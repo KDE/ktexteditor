KDE/ktexteditor

commit af729fad54f15d61ee281fea4c74575a40cebd29

Move sort implementation to C++
If the strings are a bit long the JS implementation has garbage performance.

BUG: 478250

src/utils/katecmds.cpp
@@ -610,15 +610,19 @@ KateCommands::EditingCommands::EditingCommands()
         QStringLiteral("uniq"),
         QStringLiteral("sortuniq"),
         QStringLiteral("natsort"),
+        QStringLiteral("sort"),
     })
 {
 }
 
 QList<KateCommands::EditingCommands::EditingCommand> KateCommands::EditingCommands::allCommands()
 {
-    static QList<EditingCommand> cmds{{.name = i18n("Remove Duplicate Lines"), .cmd = QStringLiteral("uniq")},
-                                      {.name = i18n("Remove Duplicates and Sort Text Alphabetically"), .cmd = QStringLiteral("sortuniq")},
-                                      {.name = i18n("Sort Text Naturally"), .cmd = QStringLiteral("natsort")}};
+    static QList<EditingCommand> cmds{
+        {.name = i18n("Remove Duplicate Lines"), .cmd = QStringLiteral("uniq")},
+        {.name = i18n("Remove Duplicates and Sort Text Alphabetically"), .cmd = QStringLiteral("sortuniq")},
+        {.name = i18n("Sort Text Naturally"), .cmd = QStringLiteral("natsort")},
+        {.name = i18n("Sort Selected Text Alphabetically"), .cmd = QStringLiteral("sort")},
+    };
     return cmds;
 }
 
@@ -670,6 +674,12 @@ bool KateCommands::EditingCommands::exec(KTextEditor::View *view, const QString 
         QCollator col;
         col.setNumericMode(true);
         std::sort(lines.begin(), lines.end(), col);
+        apply(range, doc, lines);
+        return true;
+    } else if (cmd == QStringLiteral("sort")) {
+        const auto [doc, range] = getDocAndRange();
+        QStringList lines = doc->textLines(range);
+        std::sort(lines.begin(), lines.end());
         apply(range, doc, lines);
         return true;
     }

src/script/data/commands/utils.js
@@ -3,12 +3,8 @@ var katescript = {
     "license": "LGPL-2.1+",
     "revision": 12,
     "kate-version": "5.1",
-    "functions": ["sort", "moveLinesDown", "moveLinesUp", "rtrim", "ltrim", "trim", "join", "rmblank", "alignon", "unwrap", "each", "filter", "map", "duplicateLinesUp", "duplicateLinesDown", "duplicateSelection", "rewrap", "encodeURISelection", "decodeURISelection", "fsel", "bsel"],
+    "functions": ["moveLinesDown", "moveLinesUp", "rtrim", "ltrim", "trim", "join", "rmblank", "alignon", "unwrap", "each", "filter", "map", "duplicateLinesUp", "duplicateLinesDown", "duplicateSelection", "rewrap", "encodeURISelection", "decodeURISelection", "fsel", "bsel"],
     "actions": [
-        {   "function": "sort",
-            "name": "Sort Selected Text Alphabetically",
-            "category": "Editing"
-        },
         {   "function": "rtrim",
             "name": "Remove Trailing Spaces",
             "category": "Editing"
@@ -64,11 +60,6 @@ var katescript = {
 
 // required katepart js libraries
 require ("range.js");
-
-function sort()
-{
-    each(function(lines){return lines.sort()});
-}
 
 function rtrim()
 {

src/utils/katecmds.h
@@ -219,6 +219,11 @@ public:
 
     bool
     exec(class KTextEditor::View *view, const QString &cmd, QString &errorMsg, const KTextEditor::Range &range = KTextEditor::Range(-1, -0, -1, 0)) override;
+
+    bool supportsRange(const QString &) override
+    {
+        return true;
+    }
 };
 
 } // namespace KateCommands

src/vimode/emulatedcommandbar/commandmode.cpp
@@ -48,6 +48,7 @@ CommandMode::CommandMode(EmulatedCommandBar *emulatedCommandBar,
     cmds.push_back(AppCommands::self());
     cmds.push_back(SedReplace::self());
     cmds.push_back(BufferCommands::self());
+    cmds.push_back(KateCommands::EditingCommands::self());
 
     for (KateCommandLineScript *cmd : KateScriptManager::self()->commandLineScripts()) {
         cmds.push_back(cmd);

commit cf59747e954fd6d19d87af22e53acebec12ac3d8

Fix full line selection behaviour of the C++ versions of the editing commands
These commands should operate on full lines that are part of the selection,
rather than only parts of the lines at the start and end of the selection.

Operating on only parts of the start/end lines can break those lines.

Fixes: commit af729fad54f15d61ee281fea4c74575a40cebd29
Fixes: commit 6d775e7f302d26077ff21827e33fbe4ef6efa7d0
Fixes: commit 6f539d95acc3849b5f72683cb79bbc4e5f27cdb1

src/utils/katecmds.cpp
@@ -629,11 +629,17 @@ QList<KateCommands::EditingCommands::EditingCommand> KateCommands::EditingComman
 bool KateCommands::EditingCommands::exec(KTextEditor::View *view, const QString &cmd, QString &, const KTextEditor::Range &)
 {
     auto getDocAndRange = [view]() -> std::pair<KTextEditor::Document *, KTextEditor::Range> {
-        KTextEditor::Range r = view->document()->documentRange();
+        KTextEditor::Document *doc = view->document();
         if (view->selection()) {
-            return {view->document(), view->selectionRange()};
+            KTextEditor::Range range = view->selectionRange();
+            auto start = range.start();
+            auto end = range.end();
+            start.setColumn(0);
+            end.setColumn(doc->lineLength(end.line()));
+            range.setRange(start, end);
+            return {doc, range};
         }
-        return {view->document(), r};
+        return {doc, doc->documentRange()};
     };
 
     auto apply = [](KTextEditor::Range range, KTextEditor::Document *doc, const QStringList &lines) {