eecc3d0c3bc73f131bd928d86ab280048bc41915
Kai Uwe Broulik <kde@privat.broulik.de>
2026-03-15 10:32:27 +0000

    Revert "Disable CamelCursor by default"
    This reverts commit 377eab4b92155fa60911c0dc46d758a5f90e7589

--- autotests/src/camelcursortest.cpp
+++ autotests/src/camelcursortest.cpp
@@ -19,7 +19,6 @@ CamelCursorTest::CamelCursorTest(QObject *parent)
     : QObject(parent)
 {
     doc = new KTextEditor::DocumentPrivate(false, false);
-    doc->config()->setCamelCursor(true);
     view = static_cast<KTextEditor::ViewPrivate *>(doc->createView(nullptr));
     view->resize(100, 100);
     view->show();
